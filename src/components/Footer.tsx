acf51c649ce497968a658ff53fadcb50e114dfe5
Dostanfarms <chandrasekhar3866@gmail.com>
2026-05-27 13:27:18 +0530

    new

--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-import { Facebook, Twitter, Instagram, Mail, Phone, MapPin } from "lucide-react";
-import { Button } from "@/components/ui/button";
-export const Footer = () => {
-  return <footer className="bg-card border-t">
-      
-    </footer>;
-};
